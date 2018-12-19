8a1d74f217d1492f589baed762e7e45098de410a
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-12-19 18:55:12 +0100

    Use headless Chrome for tests
    PhantomJS is deprecated

--- testem.js
+++ testem.js
@@ -8,10 +8,24 @@ module.exports = {
   before_tests: './build.js',
   test_page: 'tests/index.html?hidepassed',
   launch_in_dev: [
-    'PhantomJS',
     'Chrome'
   ],
   launch_in_ci: [
-    'PhantomJS'
-  ]
+    'Chrome'
+  ],
+  browser_args: {
+    Chrome: {
+      ci: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.CI ? '--no-sandbox' : null,
+        '--headless',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900'
+      ].filter(Boolean)
+    }
+  }
 };
